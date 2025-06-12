c459ec2dfc39a6af65751b5c5283506a7f09438d
YASH-YADAV-dynamo <yashastro23@gmail.com>
2025-06-13 03:10:36 +0530

    fix:build errors

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -12,7 +12,8 @@ import {
   PaintBrushIcon,
   ChartBarIcon,
   ClockIcon,
-  HeartIcon
+  HeartIcon,
+  ShoppingBagIcon
 } from "@heroicons/react/24/outline";
 import { 
   FireIcon as FireIconSolid,
